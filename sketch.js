5df8d4f23aa36e1b2fd55a20d965304790e967ac
aaravpareek <86913253+aaravpareek@users.noreply.github.com>
2021-07-18 17:06:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+function setup() {
+  createCanvas(400,400);
+  createSprite(200,200,20,20)
+}
+
+function draw() 
+{
+  background(30);
+
+  if (keyIsDown(DOWN_ARROW)){
+
+
+  }
+
+  if (keyIsDown(DOWN_ARROW)){
+   
+  background('red')
+
+  }
+
+}
+
+
+
+
